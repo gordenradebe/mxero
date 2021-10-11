3f26d1ef9d2453dd05dc90866311600efdbd5fa1
gordenradebe <72547436+gordenradebe@users.noreply.github.com>
2021-10-11 18:20:10 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
         projectId: "mxero-6036d",
         storageBucket: "mxero-6036d.appspot.com",
         messagingSenderId: "245317868997",
-        appId: "1:245317868997:web:f685de92ba15171ba27fac"
+        appId: "1:245317868997:web:f685de92ba15171ba27fac",
+        webpack5: false
     },
   }
